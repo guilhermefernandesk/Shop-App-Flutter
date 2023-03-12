55ad575b342213faaee2159a3e5bb728bbc881a7
Guilherme Fernandes <guilhermefernandes1937g@gmail.com>
2023-03-12 11:31:26 -0300

    first commit

---
+++ lib/utils/constants.dart
@@ -0,0 +1,9 @@
+class Constants {
+  static const userFavoritesUrl =
+      'https://shop-cod3r-953e6-default-rtdb.firebaseio.com/userFavorites';
+  static const productBaseUrl =
+      'https://shop-cod3r-953e6-default-rtdb.firebaseio.com/products';
+  static const orderBaseUrl =
+      'https://shop-cod3r-953e6-default-rtdb.firebaseio.com/orders';
+  static const webApiKey = 'AIzaSyAryCN02w0i-EX0oR_GvFs9YK2TWb4d7LQ';
+}
